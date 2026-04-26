428c5a3cdf8215a2f79c68ec4b06882ebccbb335
王东东 <20932138@qq.com>
2026-04-26 18:46:49 +0800

    调整

--- miniprogram/pages/sudoku/sudoku.ts
+++ miniprogram/pages/sudoku/sudoku.ts
@@ -7,9 +7,20 @@ Page({
   data: {
     loading: false,
     sudokuDatas: Array<string>(81).fill(''),
-    inputDatas: ['1','2','3','4','5','6','7','8','9','C'],
+    inputDatas: Array<{value: string;remain: number}>(),
+    DELETE_FLAG: 'C',
     boxColors: Array<string>(81).fill(''),
     selectedIndex: -1,
+    readableColors: Array<string>(81).fill(''),
+    disabledInputList: Array<boolean>(10).fill(false),
+    TOTAL_COUNT: 81,
+    CLASS_SELECTED: 'selected' , // 选中
+    CLASS_READABLE: 'readable',
+    CLASS_SELECTED_MARK : 'selectedMark',
+    CLASS_SAMEMARK : 'sameMark',
+    STORAGE_QUESTION_LIST: 'storageQuestionList',
+    STORAGE_RESPONSE_LIST: 'storageResponseList',
+    STORAGE_ANSWER_LIST: 'storageAnswrerList',
   },
 
   /**
@@ -17,7 +28,7 @@ Page({
    */
   onLoad() {
     this.getSudokuApi()
-    this.initBoxColors();
+    this.calBoxColors(-1, false);
   },
 
   /**
@@ -73,64 +84,87 @@ Page({
     if(this.data.loading) return;
     this.setData({ loading: true });
     wx.showLoading({ title: '加载中' })
-    const a = [5, 3, 0, 0, 7, 0, 0, 0, 0,
-      6, 0, 0, 1, 9, 5, 0, 0, 0,
-      0, 9, 8, 0, 0, 0, 0, 6, 0,
-      8, 0, 0, 0, 6, 0, 0, 0, 3,
-      4, 0, 0, 8, 0, 3, 0, 0, 1,
-      7, 0, 0, 0, 2, 0, 0, 0, 6,
-      0, 6, 0, 0, 0, 0, 2, 8, 0,
-      0, 0, 0, 4, 1, 9, 0, 0, 5,
-      0, 0, 0, 0, 8, 0, 0, 7, 9];
-    const b = a.map(t => t ? String(t):'');
-    this.setData({
-      sudokuDatas: b,
-    });
-    wx.hideLoading();
-    this.setData({ loading: false });
-    // wx.request({
-    //   url: '你的接口地址',
-    //   method: 'GET',
-    //   success: (res) => {
-    //     // 赋值渲染 grid 数据
-    //   },
-    //   complete: () => {
-    //     wx.hideLoading()
-    //     this.setData({ loading: false })
-    //   }
-    // })
+    let sudokuList = wx.getStorageSync(this.data.STORAGE_QUESTION_LIST) as string;
+    if(!sudokuList || sudokuList.length !== this.data.TOTAL_COUNT){
+      sudokuList = '530070000600195000098000060800060003400803001700020006060000280000419005000080079';
+      const answers = '534678912672195348198342567859761423426853791713924856961537284287419635345286179';
+      wx.setStorageSync(this.data.STORAGE_QUESTION_LIST, sudokuList);
+      wx.setStorageSync(this.data.STORAGE_ANSWER_LIST, answers);
+    }
+    if(!sudokuList || sudokuList.length !== this.data.TOTAL_COUNT){
+      wx.showLoading({ title: '接口异常' })
+      this.setData({ loading: false });
+    } else {
+      let sudokuDatas = sudokuList.split('').map(t => t && t !== '0' ? String(t):'');
+      const readableColors: string[] = [];
+      sudokuDatas.forEach((item) => {
+        if (item) {
+          readableColors.push(this.data.CLASS_READABLE);
+        } else {
+          readableColors.push('');
+        }
+      });
+      const responses = wx.getStorageSync(this.data.STORAGE_RESPONSE_LIST) as string;
+      if(responses && responses.length === 81){
+        sudokuDatas = responses.split('').map(t => t && t !== '0' ? String(t):'');
+      }
+      this.setData({
+        sudokuDatas,
+        readableColors,
+        loading: false,
+      });
+      this.calRemain();
+      wx.hideLoading();
+      // wx.request({
+      //   url: '你的接口地址',
+      //   method: 'GET',
+      //   success: (res) => {
+      //     // 赋值渲染 grid 数据
+      //   },
+      //   complete: () => {
+      //     wx.hideLoading()
+      //     this.setData({ loading: false })
+      //   }
+      // })
+    }
   },
   // 点击选中事件
   clickCellEvent(e: any){
     const index = e.currentTarget.dataset.index;
-    const value = this.data.sudokuDatas[index];
-    console.log(index,value);
     this.setData({
       selectedIndex: Number(index),
     });
-    this.initBoxColors();
+    this.calBoxColors(Number(index), this.data.boxColors[index]!==this.data.CLASS_SELECTED);
+    this.calRemainDisable(index);
   },
   // 点击输入
   clickInputCellEvent(e: any){
     const index = e.currentTarget.dataset.index;
+    const selectedIndex = this.data.selectedIndex;
+    if(selectedIndex > -1 && this.data.boxColors[selectedIndex]===this.data.CLASS_SELECTED && !this.data.readableColors[selectedIndex]){
+      const currentChar = this.data.inputDatas[index].value;
+      this.data.sudokuDatas[selectedIndex] = currentChar === this.data.DELETE_FLAG ? '' : currentChar;
+      this.setData({
+        sudokuDatas: this.data.sudokuDatas,
+      });
+      const answers = this.data.sudokuDatas.map(t => t ? t: '0').join('');
+      wx.setStorageSync(this.data.STORAGE_RESPONSE_LIST, answers);
+      this.calBoxColors(selectedIndex, true);
+      this.calRemain();
+      this.calRemainDisable(selectedIndex);
+    }
   },
-  // 计算每个格子属于哪个小九宫 2、4、6、8 标为 true
-  initBoxColors() {
-    const TOTAL_COUNT = 81;
-    const CLASS_SELECTED = 'selected';
-    const CLASS_SELECTED_MARK = 'selectedMark';
-    const CLASS_SAMEMARK = 'sameMark';
-    const currentSelectedIndex = this.data.selectedIndex;
+  // 设置box颜色
+  calBoxColors(currentSelectedIndex: number, selected: boolean){
     const currentChar = this.data.sudokuDatas[currentSelectedIndex];
     const currentRow = Math.floor(currentSelectedIndex/9);
     const currentCol = currentSelectedIndex%9;
     const currentBoxRow = Math.floor(currentRow / 3);
     const currentBoxCol = Math.floor(currentCol / 3);
     const currentBoxNum = currentBoxRow * 3 + currentBoxCol + 1;
-    let tmpSelected = this.data.boxColors[currentSelectedIndex]!==CLASS_SELECTED; // 是否选中了节点
     const tmpSelectedArray: number[] = []; // 选中后横竖9宫索引
     const sameCharArray: number[] = []; // 同字
-    for (let i = 0; i < TOTAL_COUNT; i++) {
+    for (let i = 0; i < this.data.TOTAL_COUNT; i++) {
       const row = Math.floor(i / 9)
       const col = i % 9
       const boxRow = Math.floor(row / 3)
@@ -140,25 +174,129 @@ Page({
       if((currentRow === row || currentCol === col || (boxNum === currentBoxNum)) && currentSelectedIndex !== i){
         tmpSelectedArray.push(i);
       }
+      // 2、4、6、8 号小九宫
+      this.data.boxColors[i] = [2, 4, 6, 8].includes(boxNum) ? 'even' : 'odd';
+
       if(this.data.sudokuDatas[i] && this.data.sudokuDatas[i] === currentChar && currentSelectedIndex !== i){
         sameCharArray.push(i);
       }
-      // 2、4、6、8 号小九宫 → 浅色
-      this.data.boxColors[i] = [2, 4, 6, 8].includes(boxNum) ? 'even' : 'odd';
     }
-    if(tmpSelected){
+    if(selected){
       // 取消则选中
-      this.data.boxColors[currentSelectedIndex] = CLASS_SELECTED;
+      this.data.boxColors[currentSelectedIndex] = this.data.CLASS_SELECTED;
       // 横竖9宫
       for(const i of tmpSelectedArray){
-        this.data.boxColors[i] = CLASS_SELECTED_MARK;
+        this.data.boxColors[i] = this.data.CLASS_SELECTED_MARK;
       }
       for(const i of sameCharArray){
         if(!tmpSelectedArray.includes(i)){
-          this.data.boxColors[i] = CLASS_SAMEMARK;
+          this.data.boxColors[i] = this.data.CLASS_SAMEMARK;
         }
       }
     }
     this.setData({ boxColors: this.data.boxColors })
   },
+  // 计算剩余
+  calRemain(){
+    const inputDatas = [{
+      value: '1',
+      remain: 9,
+    },{
+      value: '2',
+      remain: 9,
+    },{
+      value: '3',
+      remain: 9,
+    },{
+      value: '4',
+      remain: 9,
+    },{
+      value: '5',
+      remain: 9,
+    },{
+      value: '6',
+      remain: 9,
+    },{
+      value: '7',
+      remain: 9,
+    },{
+      value: '8',
+      remain: 9,
+    },{
+      value: '9',
+      remain: 9,
+    },{
+      value: this.data.DELETE_FLAG,
+      remain: 0,
+    }];
+    const inputMap = new Map<string, number>(
+      inputDatas.map(item => [item.value, item.remain])
+    );
+    for(let i = 0 ; i < this.data.TOTAL_COUNT; i ++){
+      const value = this.data.sudokuDatas[i];
+      if(value){
+        const num = inputMap.get(value) || 0;
+        inputMap.set(value, num - 1);
+      }
+    }
+    this.setData({
+      inputDatas: Array.from(inputMap).map(([value, remain]) => ({
+        value: value,
+        remain: remain
+      }))
+    });
+  },
+  // 计算可填数字
+  calRemainDisable(currentSelectedIndex: number){
+    // 题干不需要操作
+    let disabledInputList: boolean[] = [];
+    if(this.data.readableColors[currentSelectedIndex] === this.data.CLASS_READABLE){
+      disabledInputList = this.data.disabledInputList.map(_ => true);
+    } else {
+      const currentRow = Math.floor(currentSelectedIndex/9);
+      const currentCol = currentSelectedIndex%9;
+      const currentBoxRow = Math.floor(currentRow / 3);
+      const currentBoxCol = Math.floor(currentCol / 3);
+      const currentBoxNum = currentBoxRow * 3 + currentBoxCol + 1;
+      const writeNumSet = new Set<string>();
+      for (let i = 0; i < this.data.TOTAL_COUNT; i++) {
+        const row = Math.floor(i / 9)
+        const col = i % 9
+        const boxRow = Math.floor(row / 3)
+        const boxCol = Math.floor(col / 3)
+        const boxNum = boxRow * 3 + boxCol + 1
+        const tChar = this.data.sudokuDatas[i];
+        if(!tChar){
+          continue;
+        }
+        if(row === currentRow || col === currentCol || boxNum === currentBoxNum){
+          writeNumSet.add(tChar);
+        }
+      }
+      disabledInputList = this.data.disabledInputList.map((_, index) => {
+        return writeNumSet.has(this.data.inputDatas[index].value);
+      });
+      disabledInputList[disabledInputList.length-1] = false;
+    }
+    this.setData({
+      disabledInputList,
+    });
+  },
+  replay(){
+    wx.removeStorageSync(this.data.STORAGE_RESPONSE_LIST);
+    this.getSudokuApi();
+  },
+  prompt(){
+    const selectedIndex = this.data.selectedIndex;
+    if(selectedIndex > -1 && this.data.boxColors[selectedIndex]===this.data.CLASS_SELECTED && !this.data.readableColors[selectedIndex]){
+      const questions = wx.getStorageSync(this.data.STORAGE_ANSWER_LIST) as string;
+      console.log(questions);
+      if(questions && questions.length === this.data.TOTAL_COUNT){
+        wx.showToast({
+          title: questions[selectedIndex],
+          icon: 'none'
+        });
+      }
+    }
+  }
 })
