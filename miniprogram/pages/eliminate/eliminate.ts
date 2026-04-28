298ee2741a012265df94a6bf5f853c6c77cb2800
王东东 <20932138@qq.com>
2026-04-28 23:10:03 +0800

    计算行；下落；补充

--- miniprogram/pages/eliminate/eliminate.ts
+++ miniprogram/pages/eliminate/eliminate.ts
@@ -11,14 +11,15 @@ Page({
     index: -1,
     MOVE_DISTANCE: 10, // 超过这个值算移动
     state: false,
+    maxType: 5,
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-    const eliminateDatas = Array.from({ length: 81 }).map((_,index) => {
+    const eliminateDatas = Array.from({ length: 81 }).map(() => {
       return {
-        type: String(index),
+        type: String(Math.floor(Math.random() * (this.data.maxType + 1))),
         selected: false,
       }
     });
@@ -73,7 +74,6 @@ Page({
     const {clientX, clientY} = e.touches[0];
     this.data.pressInfo.startClientX = clientX;
     this.data.pressInfo.startClientY = clientY;
-    console.log(index);
     this.data.index = index;
     this.data.eliminateDatas.map((t,innerIndex) => {
       if(index ===innerIndex){
@@ -92,11 +92,11 @@ Page({
     if(!this.data.state){
       return;
     }
-    const {clientX, clientY} = e.touches[0];
+    const { clientX, clientY } = e.touches[0];
     const x = clientX - this.data.pressInfo.startClientX;
     const y = clientY - this.data.pressInfo.startClientY;
     const absX = Math.abs(x);
-    const absY = Math.abs(y)
+    const absY = Math.abs(y);
     if(absX < this.data.MOVE_DISTANCE && absY < this.data.MOVE_DISTANCE){
       return;
     }
@@ -114,7 +114,7 @@ Page({
           return t;
         });
         modifyIndex = index + 1;
-      }else if(x < 0){
+      } else if(x < 0){
         this.data.eliminateDatas.map((t,innerIndex) => {
           if(index - 1 ===innerIndex || index === innerIndex){
             t.selected = true;
@@ -125,7 +125,7 @@ Page({
         });
         modifyIndex = index - 1;
       }
-    }else if(absX < absY){
+    } else if(absX < absY){
       if(y > 0){
         this.data.eliminateDatas.map((t,innerIndex) => {
           if(index + 9 ===innerIndex || index === innerIndex){
@@ -154,10 +154,85 @@ Page({
     this.setData({
       eliminateDatas: this.data.eliminateDatas,
     });
+    // 判断行
+    for(let i = 8 ; i >= 0 ; i --){
+      const lines = this.canClearRow(i);
+      if(lines.length){
+        console.log(lines);
+        console.log(i);
+        // this.fullDownRow(i, lines);
+        // this.addZeroRow(lines);
+        setTimeout(() => {
+          this.fullDownRow(i, lines);
+        },2000);
+        setTimeout(() => {
+          this.addZeroRow(lines);
+        },4000);
+        break;
+      }
+    }
   },
   onTouchEnd(e:any) {
     const index = e.currentTarget.dataset.index;
     console.log('松开', index)
     this.data.state = false;
   },
+  // 消的优先逻辑：
+  // 行 从下到上 优先消除，消除后再比较
+  // 列 从右往左
+  // 判断行是否可以消-从9到1行
+  canClearRow(lineNo: number):number[][]{
+    // 行
+    const eliminateData = this.data.eliminateDatas.filter((_, index) => Math.floor(index / 9) === lineNo);
+    const resultArray: number[][] = [];
+    const line: number[] = [];
+    for(let i = 0 ; i < eliminateData.length ; i ++){
+      const curType = eliminateData[i].type;
+      if (!line.length) {
+        line.push(i);
+        continue;
+      }
+      if(eliminateData[line[line.length-1]].type !== curType){
+        if(line.length >= 3){
+          resultArray.push([...line]);
+        }
+        line.splice(0, line.length);
+      }
+      line.push(i);
+    }
+    if(line.length >= 3){
+      resultArray.push([...line]);
+    }
+    return resultArray;
+  },
+  fullDownRow(lineNo: number, lines: number[][]){
+    if(lineNo === 0){
+      return;
+    }
+    for(let i = lineNo ; i > 0 ; i --){
+      const startLineNo = i * 9;
+      const preLineNo = (i - 1) * 9;
+      for(let j = 0;j < lines.length ; j ++){
+        for(let t = 0; t < lines[j].length ; t ++){
+          const sLineNo = startLineNo + lines[j][t];
+          const eLineNo = preLineNo + lines[j][t];
+          this.data.eliminateDatas[sLineNo].type = this.data.eliminateDatas[eLineNo].type;
+        }
+      }
+    }
+    this.setData({
+      eliminateDatas: this.data.eliminateDatas
+    });
+  },
+  addZeroRow(lines: number[][]){
+    for(let j = 0;j < lines.length ; j ++){
+      for(let t = 0; t < lines[j].length ; t ++){
+        const sLineNo = lines[j][t];
+        this.data.eliminateDatas[sLineNo].type = String(Math.floor(Math.random() * this.data.maxType));
+      }
+    }
+    this.setData({
+      eliminateDatas: this.data.eliminateDatas
+    });
+  }
 })
